79fb65589ebbaabf25973329ce0b9c5afdb456d5
Giovane Pianco <drt.gpianco@ifood3rd.com>
2023-03-17 11:51:24 -0300

    add bread class

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify 
 
-# Create your models here.
+class Bread(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    description = models.TextField('Descrição')
+    price = models.DecimalField('Preço', max_digits=5, decimal_places=2)
+    slug = models.SlugField('Identificador', unique=True, editable=False)
+    image = models.ImageField('Imagem', upload_to='bread', blank=True, null=True)
+    weight = models.PositiveIntegerField('Peso em Kg')
+
+    class Meta:
+        verbose_name = 'Pão'
+        verbose_name_plural = 'Pães'
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.type_frame)
+        super(Bread, self).save(*args, **kwargs)
